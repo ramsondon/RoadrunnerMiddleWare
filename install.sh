110474da74923c8519b44fdf59888ec814410b0e
matthias <schmid_mat@gmx.at>
2011-10-02 16:15:26 +0200

    really added install.sh

---
+++ install.sh
@@ -0,0 +1,34 @@
+
+if [ ! -d 'lib' ]; then
+	mkdir lib
+fi
+
+cd lib
+
+##
+# @param destination directory (e.g. "doctrine")
+# @param URL of the git remote (e.g. git://github.com/doctrine/doctrine2.git)
+# @param revision to point the head (e.g. origin/HEAD)
+#
+install_git()
+{
+    INSTALL_DIR=$1
+    SOURCE_URL=$2
+    REV=$3
+
+    if [ -z $REV ]; then
+        REV=origin/HEAD
+    fi
+
+    if [ ! -d $INSTALL_DIR ]; then
+        git clone $SOURCE_URL $INSTALL_DIR
+    fi
+
+    cd $INSTALL_DIR
+    git fetch origin
+    git reset --hard $REV
+    cd ..
+}
+
+# SpiderBase 
+install_git SpiderBase https://ramsondon@github.com/ramsondon/SpiderBase-Protocol.git origin/master
